3099c8f6db4d169eef6d7f26a824ffce4e72f73e
NickWaalderbos <0341327@student.rocvantwente.nl>
2022-05-13 12:38:37 +0200

    Login/Registratie af

--- inlog.php
+++ inlog.php
@@ -5,24 +5,28 @@ include_once('connection.php');
 session_start();
 
 unset($_SESSION['error']);
-
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password']; 
-    $query = $pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
-    $query->bindParam(':username', $username);
-    $query->bindParam(':password', $password);
-  //
-    $query->execute();
-    $user = $query->fetch();
-  //
-    if ($user !== false) {
-        $_SESSION['loggedInUser'] = $user['id'];
-        header("Location: index.php");
-        die();
+if (isset($_POST['register'])) {
+    header("Location: register.php");
+}
+if (isset($_POST['login'])) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password']; 
+        $query = $pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+        $query->bindParam(':username', $username);
+        $query->bindParam(':password', $password);
+    //
+        $query->execute();
+        $user = $query->fetch();
+    //
+        if ($user !== false) {
+            $_SESSION['loggedInUser'] = $user['id'];
+            header("Location: game.php");
+            die();
+        }
+    //
+        $_SESSION['error'] = "Gebruikersnaam of wachtwoord is ongeldig.";
     }
-  //
-    $_SESSION['error'] = "Gebruikersnaam of wachtwoord is ongeldig.";
 }
 
 
@@ -34,27 +38,28 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Styles/style.css">
     <title>Log in</title>
 </head>
 <body>
-    <div class="background">
-
-    
-    <form method="POST">
-        <div>
+<div class="background">
+    <div class="loginBlock">
+        <form method="POST">
             <label for="username">Gebruikersnaam</label>
             <input type="text" name="username" id="username">
-        </div>
-        <div>
+
             <label for="password">Wachtwoord</label>
-            <input type="text" name="password" id="password">
-        </div>
+            <input type="password" name="password" id="password">
+
+            <button type="submit" class="button" name="login"><span>Log in</span></button>
+        </form>
+        <form method="POST">
+            <button type="submit" class="button" name="register"><span>Register</span></button>
+        </form>
         <?php if (isset($_SESSION['error'])) { ?>
             <div style="color: red;"><?= $_SESSION['error']; ?></div>
         <?php } ?>
-        <input type="submit" value="Inloggen">
-    </form>
     </div>
+</div>
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,81 @@
+<?php
+
+include_once('connection.php');
+
+session_start();
+
+unset($_SESSION['error']);
+if (isset($_POST['Back'])) {
+    header("Location: inlog.php");
+}
+if (isset($_POST['Register'])) {
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['password2']) && isset($_POST['E-mail'])) {
+        // SET VARIABLES
+        $checker = true;
+        $email = $_POST['E-mail'];
+        $username = $_POST['username'];
+        $password = $_POST['password']; 
+        $password2 = $_POST['password2'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $checker = false;
+        }
+        
+        if ($password !== $password2) {
+            $checker = false;
+        }
+        if ($checker == true) {
+            // QUERY
+            $query = $pdo->prepare("INSERT INTO users (username, password, email)
+            VALUES (:username, :password, :email)");
+            // BIND
+            $query->bindParam(':username', $username);
+            $query->bindParam(':password', $password);
+            $query->bindParam(':email', $email);
+    //
+        // EXECUTE QUERY
+            $query->execute();
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Styles/style.css">
+    <title>Register</title>
+</head>
+<body>
+<div class="background">
+    <div class="loginBlock">
+        <form method="POST" id="registerButton">
+            <label for="username">Gebruikersnaam</label>
+            <input type="text" name="username" id="username"> 
+
+            <label for="password">Wachtwoord</label>
+            <input type="password" name="password" id="password">
+
+            <label for="password2"> Herhaal wachtwoord</label>
+            <input type="password" name="password2" id="password2">
+
+            <label for="E-mail">E-mail</label>
+            <input type="email" name="E-mail" id="E-mail"> 
+            <div class="backButton">
+                <button class="button" type="submit" name="Register" onclick="Check()"><span>Register</span></button>
+                <button type="submit" class="button" name="Back"><span>Back</span></button>
+            </div>
+            <div class="errorSpans">
+                <span id="usernameError"></span>
+                <span id="passwordError"></span>
+                <span id="passwordMatchError"></span>
+                <span id="emailError"></span>
+                <span id='accountAanmaak'></span>
+            </div>
+        </form>
+    </div>
+</div>
+<script src='Javascript/RegisterErrors.js'></script>
+</body>
+</html>
