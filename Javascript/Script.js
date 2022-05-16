f3d23d46ecc20ef69624639e80569e90d86d70b0
W fogarin <0301944@student.rocvantwente.nl>
2022-05-16 09:51:01 +0200

    {cervios1.4}

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,20 +1,28 @@
-function toselectCH() {
-    document.getElementById("buttonBlock").style.opacity = "0";
-    document.getElementById("buttonBlock").style.zIndex = "-1";
-    document.getElementById("characterBlock").style.opacity = "1"
-    document.getElementById("characterBlock").style.zIndex = "1";
+function toBackground(i) {
+    document.getElementById(i).style.opacity = "0";
+    document.getElementById(i).style.zIndex = "-1";
 }
 
-function tomainscreen() {
-    document.getElementById("buttonBlock").style.opacity = "1";
-    document.getElementById("buttonBlock").style.zIndex = "1";
-    document.getElementById("characterBlock").style.opacity = "0"
-    document.getElementById("characterBlock").style.zIndex = "-1";
+function toForeground(i) {
+    document.getElementById(i).style.opacity = "1";
+    document.getElementById(i).style.zIndex = "1";
 }
 
-function showstats() {
-    document.getElementById("characterStats").style.opacity = "1";
-    document.getElementById("characterStats").style.zIndex = "1";
+function toSelectCharacter() {
+    toBackground("buttonBlock");
+    toForeground("characterBlock");
+}
+
+function toMainscreen() {
+    toForeground("buttonBlock");
+    toBackground("characterBlock");
+    toBackground("characterStats");
+    boxShadow(null);
+}
+
+function showStats(nameCharacter) {
+    toForeground("characterStats")
+    document.getElementById("characterName").innerText = nameCharacter;
 }
 
 function selectHero(health, armor, strength, speed, intelligence) {
@@ -25,22 +33,37 @@ function selectHero(health, armor, strength, speed, intelligence) {
     document.getElementById("intelligence").value = intelligence;
 }
 
+function boxShadow(nameCharacter) {
+    character = ["knight", "berserker", "mage", "paladin"]
+    for (let i = 0; i < character.length; i++) {
+        document.getElementById(character[i]).style.boxShadow = "0px 0px 0px black";
+    }
+    if (nameCharacter != null) {
+        document.getElementById(nameCharacter).style.boxShadow = "5px 5px 10px black";
+    }
+}
+
 function selectKnight() {
-    showstats();
     selectHero(60, 55, 40, 45, 50)
+    showStats("Knight");
+    boxShadow("knight");
 }
 
 function selectBerserker() {
     selectHero(70, 30, 65, 60, 25)
-    showstats();
+    showStats("Berserker");
+    boxShadow("berserker");
+
 }
 
 function selectMage() {
     selectHero(55, 30, 40, 45, 80)
-    showstats();
+    showStats("Mage");
+    boxShadow("mage");
 }
 
 function selectPaladin() {
     selectHero(80, 70, 50, 20, 30)
-    showstats();
+    showStats("Paladin");
+    boxShadow("paladin");
 }
--- game.php
+++ game.php
@@ -11,26 +11,30 @@
 <body>
     <div class="background">
         <div class="buttonBlock" id="buttonBlock">
-            <button class="button" onclick="toselectCH()"><span>Play</span></button>
+            <button class="button" onclick="toSelectCharacter()"><span>Play</span></button>
             <button class="button"><span>Options</span></button>
             <button class="button"><span>Exit game</span></button>
         </div>
         <div class="characterBlock" id="characterBlock">
-            <div class="gameProgressBlock">
-                <div class="           "></div>
+            <div class="gameStageBlock">
+                <div class="gameStage">Stage 1: X</div>
+                <div class="gameStage">Stage 2: X</div>
+                <div class="gameStage">Stage 3: X</div>
+                <div class="gameStage">Stage 4: X</div>
+                <div class="gameStage">Stage 5: X</div>
             </div>
-            <div class="characterSelect">
-                <button class="character" onclick="selectKnight()"><img src="Images/Knight.png" alt="KnightCH"></button>
-                <button class="character" onclick="selectBerserker()"><img src="Images/Berserker.png" alt="BerserkerCH"></button>
-                <button class="character" onclick="selectMage()"><img src="Images/Mage.png" alt="MageCH"></button>
-                <button class="character" onclick="selectPaladin()"><img src="Images/Paladin.png" alt="PaladinCH"></button>
+            <div class="characterSelect" id="characterSelect">
+                <button class="character" id="knight" onclick="selectKnight()"><img src="Images/Knight.png" alt="KnightCH"></button>
+                <button class="character" id="berserker" onclick="selectBerserker()"><img src="Images/Berserker.png" alt="BerserkerCH"></button>
+                <button class="character" id="mage" onclick="selectMage()"><img src="Images/Mage.png" alt="MageCH"></button>
+                <button class="character" id="paladin" onclick="selectPaladin()"><img src="Images/Paladin.png" alt="PaladinCH"></button>
                 <div class="chooseBlock">
                     <button class="button" onclick=""><span>Select</span></button>
-                    <button class="button" onclick="tomainscreen()"><span>Back</span></button>
+                    <button class="button" onclick="toMainscreen()"><span>Back</span></button>
                 </div>
             </div>
             <div class="characterStats" id="characterStats">
-                <div class="characterName"></div>
+                <h2 class="characterName" id="characterName"></h2>
                 <div class="statName">Health
                     <progress class="progressbar" id="health" value="100" max="100"></progress>
                 </div>
